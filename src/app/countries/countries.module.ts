72602374314e3838ecb76f036b4f19f590669a49
andres.mera <feli041089@gmail.com>
2024-03-11 14:03:24 -0500

    developer : Formulario reactivo - primer selector.

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 import { CountriesRoutingModule } from './countries-routing.module';
@@ -13,6 +14,7 @@ import { CountriesRoutingModule } from './countries-routing.module';
   imports: [
     CommonModule,
     CountriesRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class CountriesModule { }
--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class SelectorPageComponent {
 
+  public myForm: FormGroup = this.formBuilder.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    borders: ['', Validators.required],
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 }
